4ab32b5083868df61f9dbc1387a06823db1b6ece
Santhosh Kumar <santhos995@gmail.com>
2021-06-05 21:05:18 +0530

    Add project files.

---
+++ Kata.StringCalculator/StringCalculator.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Text;
+
+namespace Kata.StringCalculator
+{
+
+    /// <summary>
+    /// Followed the Kata rules from the below link :-
+    /// https://codereview.stackexchange.com/questions/128361/tdd-kata-string-calculator#:~:text=%20TDD%20-%20Kata%20-%20String%20Calculator%20,to%20refactor%20after%20each%20passing%20test%20More%20
+    /// </summary>
+    public class StringCalculator
+    {
+        string[] delimiters = new string[] { ",", "\n" };
+        public int Add(string str)
+        {
+            if (string.IsNullOrWhiteSpace(str))
+                return 0;
+            
+            modifyDelimiterIfExists(str);
+            str = removeUpUntilDelimiterIfCustomDelimiterExists(str);
+
+            string[] strSplit = str.Split(delimiters, StringSplitOptions.None);
+            return multipleValues(strSplit);
+        }
+
+        private string removeUpUntilDelimiterIfCustomDelimiterExists(string str)
+        {
+            if (str.StartsWith("//"))
+                str = str.Substring(str.IndexOf("\n") + 1);
+
+            return str;
+        }
+        private void modifyDelimiterIfExists(string str)
+        {
+            if (str.StartsWith("//"))
+            {
+                int startIndex = 2;
+                int length = str.IndexOf("\n") - startIndex;
+                delimiters = new string[] { str.Substring(startIndex, length) };
+                
+            }
+
+            
+        }
+
+        private int multipleValues(string[] strSplit)
+        {
+            int val = 0;
+            StringBuilder sb = new StringBuilder();
+            foreach (string s in strSplit)
+            {
+                if(s.StartsWith('-'))
+                {
+                    sb.Append(s + ',');
+                    continue;
+                }
+                int tmpVal = convertToInt(s);
+                if (tmpVal > 1000)
+                    continue;
+
+                val += tmpVal;
+            }
+            if (sb.Length > 0)
+                throw new ArgumentException($"Negatives not allowed - {sb}");
+            return val;
+        }
+
+        private int convertToInt(string val)
+        {
+            try
+            {
+                int res = Convert.ToInt32(val);
+                return res;
+            }
+            catch
+            {
+                throw new ArgumentException("Input is not valid");
+            }
+        }
+    }
+}
---
+++ StringCalculatorTests/StringCalculatorTest.cs
@@ -0,0 +1,85 @@
+using Kata.StringCalculator;
+using NUnit.Framework;
+using System;
+
+namespace StringCalculatorTests
+{
+    public class Tests
+    {
+        StringCalculator stringCalculator;
+        [SetUp]
+        public void Setup()
+        {
+            stringCalculator = new StringCalculator();
+        }
+
+        [Test]
+        public void Add_EmptyString_Returns0()
+        {
+            int result = stringCalculator.Add(string.Empty);
+
+            Assert.That(result == 0);
+        }
+
+        [Test]
+        public void Add_SingleValue_ReturnsValue()
+        {
+            int result = stringCalculator.Add("5");
+
+            Assert.That(result, Is.EqualTo(5));
+        }
+
+        [Test]
+        public void Add_TwoValues_ReturnsTheAddedValue()
+        {
+            int result = stringCalculator.Add("1,2");
+
+            Assert.That(result, Is.EqualTo(3));
+        }
+
+        [Test]
+        public void Add_TwoValuesWithNewLineDemiliter_ReturnsTheSum()
+        {
+            int result = stringCalculator.Add("1\n2");
+
+            Assert.That(result, Is.EqualTo(3));
+        }
+        [Test]
+        public void Add_MultipleValuesWithMultiDelimiters_ReturnsTheSum()
+        {
+            int result = stringCalculator.Add("1\n2,3");
+
+            Assert.That(result, Is.EqualTo(6));
+        }
+
+        [Test]
+        public void Add_InvalidInput_ThrowsInvalidException()
+        {
+            Assert.Throws<ArgumentException>(() => stringCalculator.Add("1,\n"));
+        }
+
+        [Test]
+        public void Add_PassCustomDelimiter_ReturnsTheSum()
+        {
+            int result = stringCalculator.Add("//;\n1;2");
+
+            Assert.That(result, Is.EqualTo(3));
+        }
+
+        [Test]
+        public void Add_PassNegativeNumber_ThrowsArgumentException()
+        {
+            var ex = Assert.Throws<ArgumentException>(() => stringCalculator.Add("1,-2"));
+            Assert.That(ex.Message, Is.EqualTo("Negatives not allowed - -2,"));
+        }
+
+        [Test]
+        public void Add_NumberWithValueMoreThan1000_ReturnsTheSumWithoutValueMoreThan1000()
+        {
+            int result = stringCalculator.Add("1,1001");
+
+            Assert.That(result, Is.EqualTo(1));
+        }
+    }
+
+}
